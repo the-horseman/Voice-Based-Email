d315cb613ed1b76c0644fbd334d761826ae36b41
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2022-05-05 16:55:18 +0530

    Updated the GUI

--- app.py
+++ app.py
@@ -7,9 +7,23 @@ r = sr.Recognizer()
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def start():
     return render_template("index.html")
+
+
+def process(text):
+    ans = ""
+    for i in text:
+        if i != ' ':
+            ans += i
+    match = "attherate"
+    for i in range(0, len(ans)-len(match)):
+        if ans[i:i+len(match):] == match:
+            ans = ans[:i:] + "@" + ans[i+len(match)::]
+    return ans
+
 
 @app.route('/convert', methods=["POST"])
 def conv():
@@ -23,21 +37,25 @@ def conv():
     with sr.AudioFile(filename) as source:
         audio_data = r.record(source)
         text = r.recognize_google(audio_data)
+        text = process(text)
         ex["data"] = text
 
     return jsonify(ex)
 
+
 @app.route("/mail", methods=["POST"])
 def ml():
     if request.method == "POST":
-        log_mail = request.form(['Log_Mail'])
-        log_pass = request.form(['Log_Pass'])
-        rec_mail = request.form(['Rec_Mail'])
-        mail_txt = request.form(['Eml_txt'])
+        data = request.get_json(force=True)
+        log_mail = data["Log_Mail"]
+        log_pass = data["Log_Pass"]
+        rec_mail = data["Rec_Mail"]
+        mail_txt = data["Eml_Text"]
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com",465,context=context) as server:
-            server.login(log_mail,log_pass)
-            server.sendmail(log_mail,rec_mail,mail_txt)
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(log_mail, log_pass)
+            server.sendmail(log_mail, rec_mail, mail_txt)
+    return jsonify(0);
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -1,163 +1,219 @@
-//webkitURL is deprecated but nevertheless
-URL = window.URL || window.webkitURL;
+window.onload = function () {
+    URL = window.URL || window.webkitURL;
 
-var gumStream; 						//stream from getUserMedia()
-var rec; 							//Recorder.js object
-var input; 							//MediaStreamAudioSourceNode we'll be recording
+    let speak = new SpeechSynthesisUtterance();
+    var log_ml = ""
+    var log_pass = ""
+    var rec_ml = ""
+    var eml_txt = ""
+    var val = 0
+    var gumStream;
+    var rec;
+    var input;
+    var current_speach
 
-// shim for AudioContext when it's not avb. 
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext //audio context to help us record
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var audioContext
 
-var recordButton = document.getElementById("recordButton");
-var stopButton = document.getElementById("stopButton");
-var pauseButton = document.getElementById("pauseButton");
+    var recordButton = document.getElementById("recordButton");
+    var stopButton = document.getElementById("stopButton");
+    var pauseButton = document.getElementById("pauseButton");
+    var au = document.getElementById("audio")
+    var upld = document.getElementById("upload")
+    var loginMail = document.getElementById("loginMail");
+    var loginPassword = document.getElementById("loginPassword");
+    var recieverMail = document.getElementById("recieverMail")
+    var emailText = document.getElementById("emailText")
+    var sendMail = document.getElementById("sendMail")
 
-//add events to those 2 buttons
-recordButton.addEventListener("click", startRecording);
-stopButton.addEventListener("click", stopRecording);
-pauseButton.addEventListener("click", pauseRecording);
+    recordButton.addEventListener("click", startRecording);
+    upld.addEventListener("click", finalise);
+    stopButton.addEventListener("click", stopRecording);
+    pauseButton.addEventListener("click", pauseRecording);
+    loginMail.addEventListener("click", login_Mail);
+    loginPassword.addEventListener("click", login_Password);
+    recieverMail.addEventListener("click", reciever_Mail);
+    emailText.addEventListener("click", email_Text);
+    sendMail.addEventListener("click", send_Mail);
 
-function startRecording() {
-	console.log("recordButton clicked");
+    function startRecording() {
+        spk("Recording");
+        speak.onend = function (blah) {
+            var constraints = { audio: true, video: false }
 
-	/*
-		Simple constraints object, for more advanced audio features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-    
-    var constraints = { audio: true, video:false }
+            recordButton.disabled = true;
+            stopButton.disabled = false;
+            pauseButton.disabled = false;
 
- 	/*
-    	Disable the record button until we get a success or fail from getUserMedia() 
-	*/
+            navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
 
-	recordButton.disabled = true;
-	stopButton.disabled = false;
-	pauseButton.disabled = false
+                audioContext = new AudioContext();
 
-	/*
-    	We're using the standard promise based getUserMedia() 
-    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
+                gumStream = stream;
 
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+                input = audioContext.createMediaStreamSource(stream);
 
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
+                rec = new Recorder(input, { numChannels: 1 })
 
-		*/
-		audioContext = new AudioContext();
+                rec.record()
 
-		//update the format 
-		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+            }).catch(function (err) {
+                console.log("Error Occured");
+            });
+        }
+    }
 
-		/*  assign to gumStream for later use  */
-		gumStream = stream;
-		
-		/* use the stream */
-		input = audioContext.createMediaStreamSource(stream);
+    function pauseRecording() {
+        if (rec.recording) {
+            rec.stop();
+            pauseButton.innerHTML = "Resume";
+        } else {
+            rec.record()
+            pauseButton.innerHTML = "Pause";
 
-		/* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
-		rec = new Recorder(input,{numChannels:1})
+        }
+    }
 
-		//start the recording process
-		rec.record()
+    function stopRecording() {
+        console.log("stopButton clicked");
 
-		console.log("Recording started");
+        stopButton.disabled = true;
+        recordButton.disabled = false;
+        pauseButton.disabled = true;
 
-	}).catch(function(err) {
-	  	//enable the record button if getUserMedia() fails
-    	recordButton.disabled = false;
-    	stopButton.disabled = true;
-    	pauseButton.disabled = true
-	});
-}
+        pauseButton.innerHTML = "Pause";
 
-function pauseRecording(){
-	console.log("pauseButton clicked rec.recording=",rec.recording );
-	if (rec.recording){
-		//pause
-		rec.stop();
-		pauseButton.innerHTML="Resume";
-	}else{
-		//resume
-		rec.record()
-		pauseButton.innerHTML="Pause";
+        rec.stop();
+        spk("Stopped Recording");
 
-	}
-}
+        gumStream.getAudioTracks()[0].stop();
 
-function stopRecording() {
-	console.log("stopButton clicked");
+        rec.exportWAV(createDownloadLink);
+    }
 
-	//disable the stop button, enable the record too allow for new recordings
-	stopButton.disabled = true;
-	recordButton.disabled = false;
-	pauseButton.disabled = true;
+    function createDownloadLink(blob) {
 
-	//reset button just in case the recording is stopped while paused
-	pauseButton.innerHTML="Pause";
-	
-	//tell the recorder to stop the recording
-	rec.stop();
+        var url = URL.createObjectURL(blob);
+        au.src = url
 
-	//stop microphone access
-	gumStream.getAudioTracks()[0].stop();
-
-	//create the wav blob and pass it on to createDownloadLink
-	rec.exportWAV(createDownloadLink);
-}
-
-function createDownloadLink(blob) {
-	
-	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var link = document.createElement('a');
-
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
-
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
-
-	//save to disk link
-	link.href = url;
-	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save to disk";
-
-	//add the new audio element to li
-	li.appendChild(au);
-	
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".wav "))
-
-	//add the save to disk link to li
-	li.appendChild(link);
-	
-	//upload link
-	var upload = document.createElement('a');
-	upload.href="#";
-	upload.innerHTML = "Upload";
-	upload.addEventListener("click", function(event){
-		  var fd=new FormData();
-		  fd.append("audio_data",blob, filename);
-          fetch("/convert", {
+        var filename = new Date().toISOString();
+        var fd = new FormData();
+        fd.append("audio_data", blob, filename);
+        fetch("/convert", {
             method: "POST",
             body: fd
-        })
-	})
-	li.appendChild(document.createTextNode (" "))//add a space in between
-	li.appendChild(upload)//add the upload link to li
+        }).then((result) => {
+            return result.json();
+        }).then((data) => {
+            current_speach = data["data"];
+            // spk("You spoke : " + current_speach);
+            console.log(val);
+            console.log(current_speach);
+            if (val == 1) {
+                document.getElementById("lgn-mail").innerText = String(current_speach);
+            }
+            else if (val == 2) {
+                document.getElementById("lgn-pass").innerText = String(current_speach);
+            }
+            else if (val == 3) {
+                document.getElementById("rec-mail").innerText = String(current_speach);
+            }
+            else if (val == 4) {
+                document.getElementById("mail-body").innerText = String(current_speach);
+            }
+        });
+    }
 
-	//add the li element to the ol
-	recordingsList.appendChild(li);
+    function showAudiobutts() {
+        recordButton.style.display = "block";
+        pauseButton.style.display = "block";
+        stopButton.style.display = "block";
+        upld.style.display = "block";
+    }
+
+    function hideAudiobutts() {
+        recordButton.style.display = "none";
+        pauseButton.style.display = "none";
+        stopButton.style.display = "none";
+        upld.style.display = "none";
+    }
+
+    function hideInputButts(exc) {
+        loginMail.disabled = true;
+        loginPassword.disabled = true;
+        recieverMail.disabled = true;
+        emailText.disabled = true;
+        exc.disabled = false;
+    }
+
+    function showInputButts() {
+        loginMail.disabled = false;
+        loginPassword.disabled = false;
+        recieverMail.disabled = false;
+        emailText.disabled = false;
+        exc.disabled = false;
+    }
+
+    function login_Mail() {
+        showAudiobutts();
+        hideInputButts(loginMail);
+        val = 1;
+        spk("You are now Recording Your Login Mail");
+    }
+    
+    function login_Password() {
+        showAudiobutts();
+        hideInputButts(loginPassword);
+        spk("You are now Recording Your Login Password");
+        alert("Remeber to input the 16 digit application specefic password. Google has now revoked the less secure login.");
+        val = 2;
+    }
+    
+    function reciever_Mail() {
+        showAudiobutts();
+        hideInputButts(recieverMail);
+        spk("You are now Recording Reciever's Mail");
+        val = 3;
+    }
+    
+    function email_Text() {
+        showAudiobutts();
+        hideInputButts(emailText);
+        spk("You are now Recording Your Email Text");
+        val = 4;
+    }
+
+    function send_Mail() {
+        spk("The Email will be sent");
+        var fd = {"Log_Mail" : log_ml, "Log_Pass" : log_pass, "Rec_Mail" : log_ml, "Eml_Text" : eml_txt};
+        fetch("/mail", {
+            method: "POST",
+            body: JSON.stringify(fd)
+        }).then();
+    }
+
+    function finalise() {
+        hideAudiobutts();
+        showInputButts();
+        if (val == 1) {
+            log_ml = current_speach;
+        }
+        if (val == 2) {
+            log_pass = current_speach;
+        }
+        if (val == 3) {
+            rec_ml = current_speach;
+        }
+        if (val == 4) {
+            eml_txt = current_speach;
+        }
+    }
+
+    function spk(txt) {
+        speak = new SpeechSynthesisUtterance();
+        speak.lang = 'en-US';
+        speak.text = txt;
+        window.speechSynthesis.speak(speak);
+    }
 }
+
